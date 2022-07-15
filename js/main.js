fc73eb57632c90a4aabda1b71b911439b116aa8b
shubhamsigdar1 <shubhamsigdar@gmail.com>
2022-07-15 09:49:47 +0530

    class weekend Boring first day

--- js/main.js
+++ js/main.js
@@ -8,10 +8,16 @@ function check() {
 
   if(day === "tuesday" || day === "thursday"){
     console.log("YOU HAVE CLASS")
+    document.querySelector('#placeToSee').innerText="YOU HAVE CLASS".toUpperCase()
+    const image = document.createElement('img')
+    image.src  = 'img/backgroundimage.jpg'
+    document.querySelector('#imagesection').appendChild(image)
   }else if( day === "saturday" || day === "sunday"){
     console.log("Its The Weekend")
+    document.querySelector('#placeToSee').innerText="Its The Weekend".toUpperCase()
   }else{
     console.log("BORING")
+    document.querySelector('#placeToSee').innerText="BORING".toUpperCase()
   }
 
 }
