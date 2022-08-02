cefe52f4a3d27bb98d2f09ebd6a9a5c246510754
shubhamsigdar1 <shubhamsigdar@gmail.com>
2022-08-02 08:48:57 +0530

    changes in javascript

--- js/main.js
+++ js/main.js
@@ -9,15 +9,31 @@ function check() {
   if(day === "tuesday" || day === "thursday"){
     console.log("YOU HAVE CLASS")
     document.querySelector('#placeToSee').innerText="YOU HAVE CLASS".toUpperCase()
+    document.querySelector('#imagesection1').classList.toggle('hidden')
     const image = document.createElement('img')
-    image.src  = 'img/backgroundimage.jpg'
-    document.querySelector('#imagesection').appendChild(image)
+    image.src  = 'img/class.jpg'
+    document.querySelector('#imagesection1').appendChild(image)
+    document.querySelector('#imagesection2').classList.add('hidden')
+    document.querySelector('#imagesection3').classList.add('hidden')
   }else if( day === "saturday" || day === "sunday"){
     console.log("Its The Weekend")
     document.querySelector('#placeToSee').innerText="Its The Weekend".toUpperCase()
-  }else{
+    document.querySelector('#imagesection2').classList.toggle('hidden')
+    const image = document.createElement('img')
+    image.src  = 'img/weekend.jpg'
+    document.querySelector('#imagesection2').appendChild(image)
+    document.querySelector('#imagesection1').classList.add('hidden')
+    document.querySelector('#imagesection3').classList.add('hidden')
+    
+  }else if( day === "monday" || day === "wednesday" || day === "friday"){
     console.log("BORING")
     document.querySelector('#placeToSee').innerText="BORING".toUpperCase()
+    document.querySelector('#imagesection3').classList.toggle('hidden')
+    const image = document.createElement('img')
+    image.src  = 'img/boring.jpg'
+    document.querySelector('#imagesection3').appendChild(image)
+    document.querySelector('#imagesection1').classList.add('hidden')
+    document.querySelector('#imagesection2').classList.add('hidden')
   }
 
 }
